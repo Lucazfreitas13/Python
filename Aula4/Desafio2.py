2ede11d1004ce9c54ca048b08fa06fd9689e0817
Lucazfreitas13 <lucas.ferreiradefreitas1995@hotmail.com>
2024-04-11 22:21:31 -0300

    Curso em Video

---
+++ Aula4/Desafio2.py
@@ -0,0 +1,4 @@
+dia=input('Informe seu dia de nascimento: ')
+mes=input('Informe seu mês de nascimento: ')
+ano=input('Informe seu ano de nascimento: ')
+print('Você nasceu no dia',dia,'de',mes,'de',ano,'. Correto?')
---
+++ Aula4/Desafio3.py
@@ -0,0 +1,3 @@
+n1=float(input('Informe o primeiro número: '))
+n2=float(input('Informe o segundo número: '))         
+print('A soma é',n1+n2)         
